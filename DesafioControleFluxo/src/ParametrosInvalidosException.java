816be91b2bb59a7e74360df6ec896d397cf1aa3b
Wanessa <132595471+wanessa-antonio@users.noreply.github.com>
2023-09-25 12:25:34 -0300

    Update ParametrosInvalidosException.java

--- DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,5 +1,5 @@
-public class ParametrosInvalidosException extends Exception {
-    public ParametrosInvalidosException(String message) {
-        super(message);
-    }
-}
+//public class ParametrosInvalidosException extends Exception {
+  //  public ParametrosInvalidosException(String message) {
+   //     super(message);
+   // }
+//}
